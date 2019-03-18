77f183f6037567bd236e5d430fb383ac682f5009
sandunsameera <sandunsameera25@gmail.com>
2019-03-18 09:18:15 +0530

    Adding Scaffold MaterialApp and material widgets

--- hello_flutter/lib/main.dart
+++ hello_flutter/lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center(
-    child: Text(
-      "Helllo sandun",
-      textDirection: TextDirection.ltr,
-    ),
-  ));
-}
+  runApp(
+      MaterialApp(
+        title: "My flutter app",
+        home: Scaffold(
+          appBar: AppBar(title: Text("This is the first screen"),),
+          body: Material(
+            color: Colors.lightBlueAccent,
+            child: Center(
+              child: Text(
+                "Helllo sandun",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(color: Colors.white, fontSize: 40.0),
+              ),
+            ),
+          ),
+        ),
+      );
+  }
