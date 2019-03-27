7dc9b012b222fd4e8a3c91cbdaa69019e711a396
sandunsameera <sandunsameera25@gmail.com>
2019-03-27 23:46:18 +0530

    use of column and row in flutter

--- ui_flutter/lib/app_screen/home.dart
+++ ui_flutter/lib/app_screen/home.dart
@@ -4,16 +4,27 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      color: Colors.deepPurple,
-      child: Text(
-        "Flight",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-            decoration: TextDecoration.none,
-            fontSize: 20.0,
-            color: Colors.lightBlue[100]),
-      ),
-    );
+        alignment: Alignment.center,
+        color: Colors.deepPurple,
+        child: Column(
+          children: <Widget>[
+            Text(
+              "Planes",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                  decoration: TextDecoration.none,
+                  fontSize: 20.0,
+                  color: Colors.lightBlue[100]),
+            ),
+            Text(
+              "Jetts",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                  decoration: TextDecoration.none,
+                  fontSize: 20.0,
+                  color: Colors.lightBlue[100]),
+            )
+          ],
+        ));
   }
 }
