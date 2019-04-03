7a77f148e05fd9a9c0c275bfbe6542929cc95392
sandunsameera <sandunsameera25@gmail.com>
2019-04-03 22:49:04 +0530

    Adding image in flutter

--- image_assets/lib/app_screens/Home.dart
+++ image_assets/lib/app_screens/Home.dart
@@ -23,11 +23,10 @@ class Home extends StatelessWidget {
                   decoration: TextDecoration.none,
                   fontSize: 20.0,
                   color: Colors.lightBlue[100]),
-            )
-            , FlightImageAssets()
+            ),
+            FlightImageAssets()
           ],
-        )
-    );//
+        )); //
   }
 }
 
@@ -36,9 +35,13 @@ class FlightImageAssets extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    AssetImage assetImage = AssetImage(
-        "images/sharp_offline_pin_black_18dp.png");
+    AssetImage assetImage =
+        AssetImage("images/sharp_offline_pin_black_18dp.png");
     Image image = Image(image: assetImage);
-    return Container(child: image,height: 300.0,width: 500.0,);
+    return Container(
+      child: image,
+      height: 300.0,
+      width: 500.0,
+    );
   }
 }
