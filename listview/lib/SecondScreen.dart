042967d551497f864f10d707730da1ead7475964
sandunsameera <sandunsameera25@gmail.com>
2019-04-05 23:47:47 +0530

    Login page

---
+++ listview/lib/SecondScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginRoute extends StatelessWidget{
+  @override
+  // ignore: missing_return
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Here"),
+      ),
+      body: Text("Login"),
+    );
+
+  }
+}
